f8b2add2b72a958d2a0b67483927a2710cf75d96
leo-MARIE <leomarie91@gmail.com>
2021-10-28 10:11:31 -0400

    final touch and favicon

--- src/components/MyHand.js
+++ src/components/MyHand.js
@@ -5,9 +5,19 @@ function MyHand(props) {
 
   return (
   <div className="hand" id="myHand">
-    <button className="buttons" id="rockbutton" value="rock" onClick={props.updateSign}></button>
-    <button className="buttons" id="paperbutton" value="paper" onClick={props.updateSign}></button>
-    <button className="buttons" id="scissorsbutton" value="scissors" onClick={props.updateSign}></button>
+    <div>
+      <button className="buttons" id="rockbutton" value="rock" onClick={props.updateSign}></button>
+      <br/>
+      <div>Rock</div>
+    </div>
+    <div>
+      <button className="buttons" id="paperbutton" value="paper" onClick={props.updateSign}></button>
+      <div>Paper</div>
+    </div>
+    <div>
+      <button className="buttons" id="scissorsbutton" value="scissors" onClick={props.updateSign}></button>
+      <div>Scissors</div>
+    </div>
   </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import rock from './../image/rock150.png';
-import paper from './../image/paper150.png';
-import scissors from './../image/scissors150.png';
+import rock from './../image/rock200.png';
+import paper from './../image/paper200.png';
+import scissors from './../image/scissors200.png';
 
 function Table(props) {
 
 let oppImage;
 if(props.signs.oppSign === "rock") {
-  oppImage = <img src={rock} alt="Rock" width="150" height="150"></img>
+  oppImage = <img src={rock} alt="Rock"></img>
 } else if(props.signs.oppSign === "paper") {
-  oppImage = <img src={paper} alt="Paper" width="150" height="150"></img>
+  oppImage = <img src={paper} alt="Paper"></img>
 } else if(props.signs.oppSign === "scissors") {
-  oppImage = <img src={scissors} alt="Scissors" width="150" height="150"></img>
+  oppImage = <img src={scissors} alt="Scissors"></img>
 }
 
 let myImage;
 if(props.signs.mySign === "rock") {
-  myImage = <img src={rock} alt="Rock" width="150" height="150"></img>
+  myImage = <img src={rock} alt="Rock"></img>
 } else if(props.signs.mySign === "paper") {
-  myImage = <img src={paper} alt="Paper" width="150" height="150"></img>
+  myImage = <img src={paper} alt="Paper"></img>
 } else if(props.signs.mySign === "scissors") {
-  myImage = <img src={scissors} alt="Scissors" width="150" height="150"></img>
+  myImage = <img src={scissors} alt="Scissors"></img>
 }
 
   return (
@@ -29,7 +29,6 @@ if(props.signs.mySign === "rock") {
         <div className="imageWrapper">
           {oppImage}
         </div>
-        <br/>
         <br/>
         <br/>
         <br/>
