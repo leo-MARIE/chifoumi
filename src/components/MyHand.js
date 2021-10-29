afd6018aef2a35815073519b086df3653cbb972d
leo-MARIE <leomarie91@gmail.com>
2021-10-29 13:05:04 -0400

    added robots.txt

--- src/components/MyHand.js
+++ src/components/MyHand.js
@@ -6,16 +6,16 @@ function MyHand(props) {
   return (
   <div className="hand" id="myHand">
     <div>
-      <button className="buttons" id="rockbutton" value="rock" onClick={props.updateSign}></button>
+      <button className="buttons" id="rockbutton" value="rock" aria-label="rock" onClick={props.updateSign}></button>
       <br/>
       <div>Rock</div>
     </div>
     <div>
-      <button className="buttons" id="paperbutton" value="paper" onClick={props.updateSign}></button>
+      <button className="buttons" id="paperbutton" value="paper" aria-label="paper" onClick={props.updateSign}></button>
       <div>Paper</div>
     </div>
     <div>
-      <button className="buttons" id="scissorsbutton" value="scissors" onClick={props.updateSign}></button>
+      <button className="buttons" id="scissorsbutton" value="scissors" aria-label="scissors" onClick={props.updateSign}></button>
       <div>Scissors</div>
     </div>
   </div>
